6b511705104bda2ac1f92afffa9f09e17b3743d7
fedirky <fedirky@gmail.com>
2024-12-03 04:03:15 +0200

    limit added

--- graveyard.gd
+++ graveyard.gd
@@ -2,6 +2,7 @@ extends GridMap
 
 @export var sceleton: PackedScene
 var spawners = []
+var spawnedSceletons = 0
 
 
 func _ready() -> void:
@@ -11,6 +12,10 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+    print(Dialogic.VAR.raidStart)
+    if Dialogic.VAR.raidStart == true:
+        $Timer.start()
+        Dialogic.VAR.raidStart = false
     pass
 
 
@@ -19,3 +24,6 @@ func _on_timer_timeout() -> void:
     sceleton_instance.player = $"../mage"
     sceleton_instance.global_position = spawners.pick_random().global_position
     add_child(sceleton_instance)
+    spawnedSceletons += 1
+    if spawnedSceletons == 10:
+        $Timer.stop()
